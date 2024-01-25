c0cfcd56ca3423e8be7cb5579ab2e68e50ec3178
Felipe Teixeira <felipesantosteixeira@outlook.com>
2024-01-25 16:31:51 -0300

    new details

--- src/app/shared/sections/section-info/section-info.component.ts
+++ src/app/shared/sections/section-info/section-info.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-section-info',
-  templateUrl: './section-info.component.html',
-  styleUrls: ['./section-info.component.scss']
+    selector: 'app-section-info',
+    templateUrl: './section-info.component.html',
+    styleUrls: ['./section-info.component.scss'],
 })
 export class SectionInfoComponent {
+    password = 'fernandocarros';
+    showMessage = false;
 
+    async copy() {
+        try {
+            this.showMessage = true;
+            await navigator.clipboard.writeText(this.password);
+            setTimeout(() => {
+                this.showMessage = false;
+            }, 2000);
+        } catch (err) {
+            console.error('Erro ao copiar texto:', err);
+        }
+    }
 }

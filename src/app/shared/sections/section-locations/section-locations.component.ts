02dc9d03847d910ba29639d41fe2af95febabc55
Felipe Teixeira <felipesantosteixeira@outlook.com>
2024-01-29 17:15:16 -0300

    localizacao

--- src/app/shared/sections/section-locations/section-locations.component.ts
+++ src/app/shared/sections/section-locations/section-locations.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-section-locations',
-  templateUrl: './section-locations.component.html',
-  styleUrls: ['./section-locations.component.scss']
+    selector: 'app-section-locations',
+    templateUrl: './section-locations.component.html',
+    styleUrls: ['./section-locations.component.scss'],
 })
 export class SectionLocationsComponent {
-
+    locations = [
+        {
+            name: 'Mercearia do Goiano',
+            time: 'fica na vizinhança da casa',
+            icon: 'market',
+        },
+        {
+            name: 'Mercado Tida',
+            address: 'Alameda Cristóvão de Barros, 626 - Porto Novo',
+            time: '3 min',
+            distance: '900 m',
+            logo: 'logo-tida.jpeg',
+            link: 'https://maps.app.goo.gl/P1n8mS9NNaVeTd3MA',
+        },
+        {
+            name: 'Mercado Piratininga',
+            address: 'Av. José Herculano, 7040 - Travessão',
+            time: '7 min',
+            distance: '2,7 km',
+            logo: 'logo-piratininga.jpeg',
+            link: 'https://maps.app.goo.gl/q2wM7LHcimiKYcHV9',
+        },
+        {
+            name: 'Mercado Atacadão',
+            address: 'Rodovia Rio-Santos BR 101 KM 111 - Travessão',
+            time: '11 min',
+            distance: '4,1 km',
+            logo: 'logo-atacadao.jpeg',
+            link: 'https://maps.app.goo.gl/dFkSqX5WPeAE2L6GA',
+        },
+        {
+            name: 'Pizzaria Lunamar',
+            time: 'fica na vizinhança da casa',
+            logo: 'logo-lunamar.jpeg',
+        },
+        {
+            name: 'Surfs Pizzaria choperia e Restaurante',
+            address: 'R. Padre Anchieta, 931 - Centro',
+            time: '14 min',
+            distance: '8,5 km',
+            logo: 'logo-surfs.jpeg',
+            link: 'https://maps.app.goo.gl/SWQwChnReCwjDXTaA',
+        },
+    ];
 }
